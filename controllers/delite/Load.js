4625503bb90d11c0645c4372a15fc356ba320a7d
Christophe Jolif <cjolif@gmail.com>
2013-11-26 15:16:39 +0100

    progress on delite controllers

--- controllers/delite/Load.js
+++ controllers/delite/Load.js
@@ -1,19 +1,29 @@
-define(["dcl/dcl", "dojo/_base/lang", "../../Controller"], function (dcl, lang, Controller) {
-
+define(["require", "dcl/dcl", "dojo/_base/lang", "dojo/Deferred", "../../Controller"],
+	function (require, dcl, lang, Deferred, Controller) {
 	return dcl(Controller, {
 		constructor: function () {
 			document.addEventListener("delite-load", lang.hitch(this, "_loadHandler"), true);
 		},
 		_loadHandler: function (event) {
 			// load the actual view
-			// where do I put it?? the target is the container?
-			// two use-cases: if the container is a transition container, he is the one that fired the load event,
-			// he will manage the addition for us?
-			// if the container is a regular container?
-			event.loadDeferred.resolve();
+			// TODO: don't I have two cases here, when the parent is a delite display container and when not?
+			var viewId = event.dest || "";
+			var parts = viewId.split(",");
+			var childId = parts.shift();
+			var params = event.params || "";
+			this._createView(event.loadDeferred, name, params, parent.views[childId].type);
+		},
+
+		_createView: function (deferred, name, params, type) {
+			var app = this.app;
+			require([type ? type : "../View"], function (View) {
+				var newView = new View(dcl.mixin({
+					"app": app,
+					"id": name
+					// "parent": parent
+				}, { "params": params }));
+				deferred.resolve(newView);
+			});
 		}
 	});
 });
-
-
-
--- controllers/delite/Transition.js
+++ controllers/delite/Transition.js
@@ -1,40 +1,47 @@
-define(["dcl/dcl", "dojo/_base/lang", "../../Controller"], function (dcl, lang, Controller) {
+define(["dcl/dcl", "dojo/Deferred", "dojo/when", "dojo/promise/all", "dojo/_base/lang", "../../Controller"],
+	function (dcl, Deferred, when, all, lang, Controller) {
 
 	return dcl(Controller, {
 		constructor: function () {
 			document.addEventListener("delite-display", lang.hitch(this, "_displayHandler"), true);
 		},
 		_displayHandler: function (event) {
-			var views = event.dest.split("+");
-			for (var i = 0; i < views.length; i++) {
-				var parentView = this.getParentViewFromTarget(views[i]);
-
-			}
-			if (destContainer !== event.target) {
-				event.stopPropagation();
-				destContainer.emit("delite-display", event);
-			}
-		},
-		getParentFromViewTarget: function (viewTarget) {
-			var p = this.app.views;
-			var parts = viewTarget.split(",");
-			if (parts.length > 1) {
-				for (var i = 0; i < parts.length - 1; i++) {
-					var toId = parts[i];
-					var v = p[toId];
-					if (v) {
-						p = v;
-					} else {
-						console.log("Parent view has not been created yet for " + viewTarget);
-						return null;
-					}
+			if (event.target === document) {
+				var views = event.dest.split("+");
+				for (var i = 0; i < views.length; i++) {
+					// display the view
+					this._displayView(views[i], event);
 				}
 			}
-			console.log("in getParentFromViewTarget for viewTarget [" + viewTarget + "] parent [" + p.id + "]");
-			return p;
+		},
+		_displayView: function (viewTarget, event) {
+			var deferred = new Deferred(), subEvent;
+			// wait for parents to be displayed first
+			when(this._displayParents(viewTarget, event), function (parent) {
+				// event.stopPropagation();
+				// parent is the view, the container is only child of the view
+				// TODO make sure one can in the config of the view specificy a different container
+				// "myview": { container: "a query string" }
+				// and when specified use the query string here to get the container instead of the only child
+				subEvent = Object.create(event);
+				subEvent.transitionDeferred = deferred;
+				if (parent !== null) {
+					parent.children[0].emit("delite-display", subEvent);
+				} else {
+					// TODO: we are a top level view?
+				}
+			});
+			return deferred.promise;
+		},
+		_displayParents: function (viewTarget, event) {
+			// for now we consider the parents are listed in the display command (i.e. parent1,parent2,view)
+			// TODO: we might improve that later to avoid users have to specify this?
+			var parts = viewTarget.split(",");
+			if (parts.length > 1) {
+				parts.pop();
+				return this._displayView(parts.join(","), event);
+			}
+			return null;
 		}
 	});
 });
-
-
-
